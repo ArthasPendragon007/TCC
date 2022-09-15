4521f12ce44fd8822daece92d46290921353370f
Augusto Carvalho <augustobcarv@gmail.com>
2022-09-14 21:06:24 -0300

    third commit

--- Jogo/Jogo/Script/Fall_zone.gd
+++ Jogo/Jogo/Script/Fall_zone.gd
@@ -5,8 +5,9 @@ extends Area2D
 # var a = 2
 # var b = "text"
 
-
-
+var FakeInterface = load("res://Levels/scene/GAME OVER/game_over.tscn").instance()
 func _on_Fall_zone_body_entered(body):
+
 	print (body)
-	get_tree ().reload_current_scene()
+	get_tree().paused = !get_tree().paused
+	get_tree().current_scene.add_child(FakeInterface)
